e188fe168d3eb68b8a14da39cf76f53bc073f5b8
Chandan Shaw <113237656+chandanshaw833609@users.noreply.github.com>
2023-11-02 07:22:11 +0530

    Update main.js

--- projects/09AlarmClock/main.js
+++ projects/09AlarmClock/main.js
@@ -2,15 +2,13 @@ function currentTime(){
     const date = new Date()
     let hour = date.getHours()
     let minute = date.getMinutes()
-    let sec = date.getSeconds()
     if (hour >= 12) hour = hour - 12
     const time = document.getElementById('time')
-    time.innerHTML = `${hour}:${minute}:${sec}`
+    time.innerHTML = `${hour}:${minute}`
     const alarmTime = document.getElementById("alarm_time")
     
     const hour1 = document.getElementById("hour"),
-    minute1 = document.getElementById("minute"),
-    sec1 = document.getElementById("sec")
+    minute1 = document.getElementById("minute")
 
     for (let i = 1; i <= 12; i++) {
         hour1.insertAdjacentHTML("beforeend",`<option value="${i}">${i}</option>`)
@@ -19,14 +17,9 @@ function currentTime(){
     for (let i = 0; i <= 60; i++) {
         minute1.insertAdjacentHTML("beforeend",`<option value="${i}">${i}</option>`)
     }
-
-    for (let i = 0; i < 60; i++) {
-        sec1.insertAdjacentHTML("beforeend",`<option value="${i}">${i}</option>`)
-    }
-
     const alarmAudio = new Audio("https://www.sjap.nl/Take-off-warning.mp3")
 
-    if (hour == hour1.value && minute == minute1.value && sec == sec1.value) {
+    if (hour == hour1.value && minute == minute1.value) {
         alarmAudio.play()
         alarmAudio.loop = true
     }

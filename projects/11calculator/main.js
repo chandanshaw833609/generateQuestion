1e55c465600ad97995fff441af5fc0596c8dff4d
Chandan Shaw <113237656+chandanshaw833609@users.noreply.github.com>
2023-11-05 10:41:39 +0530

    Update main.js

--- projects/11calculator/main.js
+++ projects/11calculator/main.js
@@ -1,21 +1,42 @@
 const btnCss = document.querySelectorAll(".btn_css")
 const written_value = document.querySelector("#written_value")
 const calculated_value = document.querySelector("#calculated_value")
+let openBracket = 0
+console.log(openBracket + " openBracket");
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < btnCss.length; i++) {
     btnCss[i].addEventListener('click', function() {
         written_value.textContent  = written_value.textContent + btnCss[i].value
     })
 }
 
 function calculatedValue () {
-    const calValue = eval(written_value.textContent)
+    try {
+        let operations = "*/-+"
+    
+        for (let i=0; i<=written_value.textContent.length; i++) {
+
+            if (written_value.textContent[i] === "(" && !operations.includes(written_value.textContent[i-1]) && i!=0 && written_value.textContent[i-1] !=="(") {
+                written_value.textContent = written_value.textContent.substring(0,i) + "*" + written_value.textContent.substring(i,written_value.textContent.length)
+            }
+
+            if (openBracket > 0) {
+                written_value.textContent = written_value.textContent + ")"
+                openBracket--;
+            }
+        }
+    const calValue = eval(written_value.textContent).toFixed(3)
     calculated_value.textContent = calValue
+    } catch (error) {
+        calculated_value.textContent = "Syntax Error"
+    }
+    
 }
 
 function deleteLastDigit() {
     let w_value = written_value.textContent
     let c_value = calculated_value.textContent
+    if (written_value.textContent[written_value.textContent.length-1] === "(") openBracket--;
     written_value.textContent =  w_value.substring(0,w_value.length-1)
     if (w_value == "") calculated_value.textContent = c_value.substring(0,c_value.length-1)
 }
@@ -23,30 +44,39 @@ function deleteLastDigit() {
 function allClear () {
     written_value.textContent = ""
     calculated_value.textContent = ""
+    openBracket = 0
 }
 
 function bracket() {
     let value = written_value.textContent
-    let pbfb 
-    // pbfb = position before first bracket "("
     
     for (let i  = value.length; i>=0; i--) {
-        if (value[i] == "(") {
-            written_value.textContent = value + ")"
-            pbfb = i-1
-            console.log(pbfb);
-            if ((value[pbfb] != "*" || value[pbfb] != "/" || value[pbfb] != "+" || value[pbfb] != "-") && pbfb !=-1 ) {
-                written_value.textContent = written_value.textContent.substring(0,pbfb+1) + "*" + written_value.textContent.substring(pbfb+1,written_value.textContent.length)
-            }
-            break
-        } 
-        else if (value[i] == ")" || !value.includes("(")) {
+        let numbers = "0123456789"
+        
+        if (value[i] =="*" || value[i] =="/" || value[i] =="+" || value[i] =="-") {
             written_value.textContent = value + "("
+            openBracket += 1
             break
-        } else if (value.length == 0) {
-            written_value.textContent = "("
         }
+        else if (value[i] ===")" && openBracket == 0) {
+            written_value.textContent = value + "("
+            openBracket += 1
+            break
+        }
+        else if (numbers.includes(value[i]) && openBracket == 0) {
+            written_value.textContent = value + "("
+            openBracket += 1
+            break
+        } else if (openBracket > 0 && numbers.includes(value[i])) {
+            written_value.textContent = value + ")"
+            openBracket --;
+            break
+        } else if (written_value.textContent.length == 0) {
+            written_value.textContent = "("
+            openBracket = 1
+        } 
     }
+    
 
 }
 
